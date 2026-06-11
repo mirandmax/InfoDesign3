8d50667e9932a0ba0061565d23882e312306fda5
Punti <elias.puntaier@hotmail.com>
2026-06-11 13:50:42 +0200

    added gdp per capita to data.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   loadCSV(
     "data/tax-revenues-vs-income-inequality/tax-revenues-vs-income-inequality.csv",
+    "data/gdp-per-capita-worldbank/gdp-per-capita-worldbank.csv"
   ).then((rows) => {
     const year = 2021;
     const currentYearsData = rows
