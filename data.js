22f32696163ae22450efae30569327628b9fe8ea
Punti <elias.puntaier@hotmail.com>
2026-06-11 11:26:26 +0200

    added the first working implementation of choropleth map

--- data.js
+++ data.js
@@ -1,12 +1,12 @@
 async function loadCSV(path) {
-  const raw = await d3.csv(path, d => ({
+  const raw = await d3.csv(path, d => {return ({
     country:   d['Entity'],
     code:      d['Code'],
     year:      +d['Year'],
     gini:      d['Gini coefficient']  !== '' ? +d['Gini coefficient']  : null,
     taxPct:    d['Tax revenues (% of GDP)']   !== '' ? +d['Tax revenues (% of GDP)']   : null,
     continent: d['World region according to OWID'],
-  }));
+  })});
 
   const clean = raw.filter(d => d.year && d.country);
   console.log(`Loaded ${clean.length} rows (inkl. Zeilen mit nur einem Wert)`);
