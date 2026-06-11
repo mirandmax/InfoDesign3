8ef0e468c7b42fda8d526198f21a7f4ea8f3b771
Punti <elias.puntaier@hotmail.com>
2026-06-11 11:10:06 +0200

    added loadCSV

---
+++ choropleth-map.js
@@ -0,0 +1,52 @@
+function drawChoroplethMap(data) {
+  const el = document.getElementById("choropleth-map");
+  if (!el) return;
+
+  const width = 800,
+    height = 600;
+
+  const svg = d3
+    .select(el)
+    .append("svg")
+    .attr("viewBox", `0 0 ${width} ${height}`)
+    .attr("width", width)
+    .attr("height", height);
+
+  const g = svg.append("g");
+  //End of Configuration
+
+  const path = d3.geoPath();
+  const projection = d3
+    .geoMercator()
+    .scale(70)
+    .center([0, 20])
+    .translate([width / 2, height / 2]);
+
+  // Data and color scale
+  const colorScale = d3
+    .scaleThreshold()
+    .domain([0.35, 0.4, 0.45, 0.5, 0.55])
+    .range(d3.schemeReds[7]);
+  // Load external data and boot
+  Promise.all([
+    d3.json(
+      "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson",
+    ),
+  ]).then(function (loadData) {
+    let topo = loadData[0];
+
+    // Draw the map
+    svg
+      .append("g")
+      .selectAll("path")
+      .data(topo.features)
+      .join("path")
+      // draw each country
+      .attr("d", d3.geoPath().projection(projection))
+      // set the color of each country
+      .attr("fill", function (d) {
+        d.total = ChoroData.get(d.id) || 0;
+        return colorScale(d.total);
+      });
+  });
+}
--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-
-document.addEventListener('DOMContentLoaded', () => {  
-  loadCSV('resources/tax-revenues-vs-income-inequality.csv').then(rows => {
+document.addEventListener("DOMContentLoaded", () => {
+  loadCSV("data/tax-revenues-vs-income-inequality/tax-revenues-vs-income-inequality.csv").then((rows) => {
+    console.log(rows);
     const year = 2023;
-    
-    drawChoropolethMap(rows)
+    const mockData = [{ Entity: "Italy", gini: 0.33 }, { Entity: "Germany", gini: 0.29 }, { Entity: "United States", gini: 0.41 }];
+    drawChoropolethMap(mockData);
   });
-
 });
