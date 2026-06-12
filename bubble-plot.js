c2ff57567d7d4725a7825bd69ba264bb61df6424
Max Mirandola <max.mirandola@ciloud.com>
2026-06-12 12:10:29 +0200

    simple slider for bubble plot

--- bubble-plot.js
+++ bubble-plot.js
@@ -1,51 +1,58 @@
 // set the dimensions and margins of the graph
-var margin = { top: 10, right: 150, bottom: 30, left: 50 },
-  width = 700 - margin.left - margin.right,
-  height = 420 - margin.top - margin.bottom;
+var marginBubble = { top: 30, right: 150, bottom: 40, left: 50 },
+  widthBubble = 900 - marginBubble.left - marginBubble.right,
+  heightBubble = 420 - marginBubble.top - marginBubble.bottom;
 
 // append the svg object to the body of the page
 const svg = d3.select("#bubble-plot")
   .append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
+  .attr("width", widthBubble + marginBubble.left + marginBubble.right)
+  .attr("height", heightBubble + marginBubble.top + marginBubble.bottom)
   .append("g")
-  .attr("transform", `translate(${margin.left},${margin.top})`);
+  .attr("transform", `translate(${marginBubble.left},${marginBubble.top})`);
 
 //Read the data
 //d3.csv("data/tax-revenues-vs-income-inequality/tax-revenues-vs-income-inequality.csv").then(function (data) {
 function drawBubblePlot(data) {
-  data = data.filter(d => d.gini !== null && d.taxPct !== null && d.population !== null);
-
-  currentYear = 2021;
-  data = data.filter(d => +d.year === currentYear);
-
-  xmax = d3.max(data, d => d.gini);
-  ymax = d3.max(data, d => d.taxPct);
-  rmin = d3.min(data, d => d.population);
-  rmax = d3.max(data, d => d.population);
-  
-  console.log(data.filter(d  => d.continent === "Asia"));
-
-  // Add X axis
-  const x = d3.scaleLinear()
-    .domain([0, xmax * 1.1])
-    .range([0, width]);
-  svg.append("g")
-    .attr("transform", `translate(0, ${height})`)
-    .call(d3.axisBottom(x));
-
-  // Add Y axis
-  const y = d3.scaleLinear()
-    .domain([0, ymax * 1.1])
-    .range([height, 0]);
-  svg.append("g")
-    .call(d3.axisLeft(y));
+  const cleanData = data.filter(d => d.gini !== null && d.taxPct !== null && d.population !== null && d.gdpPerCapita !== null);
+  const years = cleanData.map(d => +d.year);
+  const minYear = d3.min(years);
+  const maxYear = d3.max(years);
+  const slider = d3.select("#year-slider");
+  const initialYear = Math.max(minYear, Math.min(maxYear, +slider.property("value") || maxYear));
 
 
-  // Add a scale for bubble size
-  const z = d3.scaleLinear()
-    .domain([rmin, rmax])
-    .range([2, 35]);
+  slider
+    .attr("min", minYear)
+    .attr("max", maxYear)
+    .property("value", initialYear);
+
+  const chartLayer = svg.append("g").attr("class", "bubble-chart");
+  const xAxis = chartLayer.append("g")
+    .attr("class", "x-axis")
+    .attr("transform", `translate(0, ${heightBubble})`);
+  const yAxis = chartLayer.append("g")
+    .attr("class", "y-axis");
+
+  chartLayer.append("text")
+    .attr("class", "x-axis-label")
+    .attr("text-anchor", "end")
+    .attr("x", widthBubble)
+    .attr("y", heightBubble + marginBubble.top + 10)
+    .text("Gini Coefficient");
+
+  chartLayer.append("text")
+    .attr("class", "y-axis-label")
+    .attr("text-anchor", "start")
+    .attr("x", -10)
+    .attr("y", -10)
+    .text("Tax Revenue");
+
+  const bubbleLayer = chartLayer.append("g").attr("class", "bubble-layer");
+
+  const x = d3.scaleLinear().range([0, widthBubble]);
+  const y = d3.scaleLinear().range([heightBubble, 0]);
+  const z = d3.scaleLinear().range([2, 35]);
 
   // Add a scale for bubble color
   var myColor = d3.scaleOrdinal()
@@ -56,14 +63,11 @@ function drawBubblePlot(data) {
   //Tooltip
 
   // Create a tooltip div that is hidden by default:
-  const tooltip = d3.select("#bubble-plot")
+  const tooltip = d3.select("body")
     .append("div")
+    .style("position", "absolute")
     .style("opacity", 0)
-    .attr("class", "tooltip")
-    .style("background-color", "black")
-    .style("border-radius", "5px")
-    .style("padding", "10px")
-    .style("color", "white")
+    .attr("class", "tooltip");
 
   //Create 3 functions to show / update (when mouse move but stay on same circle) / hide the tooltip
   const showTooltip = function (event, d) {
@@ -73,26 +77,23 @@ function drawBubblePlot(data) {
       .duration(200)
     tooltip
       .style("opacity", 1)
-      .html("Country: " + d.country)
-      .style("left", (event.x) / 2 + "px")
-      .style("top", (event.y) / 2 - 50 + "px")
+      .html(d.country)
+      .style("left", (event.pageX + 10) + "px")
+      .style("top", (event.pageY + 10) + "px")
   }
   const moveTooltip = function (event, d) {
     tooltip
-      .style("left", (event.x) / 2 + "px")
-      .style("top", (event.y) / 2 - 50 + "px")
+      .style("left", (event.pageX + 10) + "px")
+      .style("top", (event.pageY + 10) + "px")
   }
   const hideTooltip = function (event, d) {
     tooltip
-      .transition()
-      .duration(200)
       .style("opacity", 0)
   }
 
   //Highlight a group
   const highlight = function (event, d) {
     // reduce opacity of all groups
-    console.log(d);
     d3.selectAll(".bubbles").style("opacity", .05)
     // expect the one that is hovered
     d3.selectAll("." + d.replace(/\s/g, '')).style("opacity", 1)
@@ -103,26 +104,64 @@ function drawBubblePlot(data) {
     d3.selectAll(".bubbles").style("opacity", 1)
   }
 
-  // Add dots
-  svg.append('g')
-    .selectAll("dot")
-    .data(data)
-    .join("circle")
-    .attr("class", function (d) { return "bubbles " + d.continent.replace(/\s/g, '') })
-    .attr("cx", d => x(d.gini))
-    .attr("cy", d => y(d.taxPct))
-    .attr("r", d => z(d.population))
-    .style("fill", d => myColor(d.continent))
-    //.style("opacity", "0.7")
-    //.attr("stroke", "black")
-    .on("mouseover", showTooltip)
-    .on("mousemove", moveTooltip)
-    .on("mouseleave", hideTooltip)
+  function update(selectedYear) {
+    const filteredData = cleanData.filter(d => +d.year === +selectedYear);
+
+    const bubbles = bubbleLayer.selectAll("circle")
+      .data(filteredData, d => d.country);
+
+    bubbles.exit()
+      .transition()
+      .duration(200)
+      .style("opacity", 0)
+      .remove();
+
+    const bubblesEnter = bubbles.enter()
+      .append("circle")
+      .attr("class", d => "bubbles " + d.continent.replace(/\s/g, ''))
+      .attr("cx", d => x(d.gini))
+      .attr("cy", d => y(d.taxPct))
+      .attr("r", d => z(d.gdpPerCapita))
+      .style("fill", d => myColor(d.continent))
+      .style("opacity", 1)
+      .on("mouseover", showTooltip)
+      .on("mousemove", moveTooltip)
+      .on("mouseleave", hideTooltip);
+
+    const bubblesUpdate = bubblesEnter.merge(bubbles);
+
+    bubblesUpdate
+    .transition()
+    .duration(300)
+    .style("opacity", 1)
+      .attr("cx", d => x(d.gini))
+      .attr("cy", d => y(d.taxPct))
+      .attr("r", d => z(d.gdpPerCapita))
+      .style("fill", d => myColor(d.continent));
+  }
+
+  slider.on("input", function () {
+    update(+this.value);
+  });
+
+
+  const xmin = d3.min(cleanData, d => d.gini);
+  const xmax = d3.max(cleanData, d => d.gini);
+  const ymax = d3.max(cleanData, d => d.taxPct);
+  const rmin = d3.min(cleanData, d => d.gdpPerCapita);
+  const rmax = d3.max(cleanData, d => d.gdpPerCapita);
+
+  x.domain([xmin * 0.9, xmax * 1.1]);
+  y.domain([0, ymax * 1.1]);
+  z.domain([rmin, rmax]);
+
+  xAxis.transition().duration(500).call(d3.axisBottom(x));
+  yAxis.transition().duration(500).call(d3.axisLeft(y));
+
+  update(initialYear);
 
 
   //legend
-  // Add legend: circles
-
 
   // Add one dot in the legend for each name.
   const size = 20
@@ -130,8 +169,8 @@ function drawBubblePlot(data) {
   svg.selectAll("myrect")
     .data(allgroups)
     .join("circle")
-    .attr("cx", width)
-    .attr("cy", (d, i) => 10 + i * (size + 5)) // 100 is where the first dot appears. 25 is the distance between dots
+    .attr("cx", widthBubble * 0.9)
+    .attr("cy", (d, i) => 10 + i * (size + 5))
     .attr("r", 7)
     .style("fill", d => myColor(d))
     .on("mouseover", highlight)
@@ -142,8 +181,8 @@ function drawBubblePlot(data) {
     .data(allgroups)
     .enter()
     .append("text")
-    .attr("x", width + size * .8)
-    .attr("y", (d, i) => i * (size + 5) + (size / 2)) // 100 is where the first dot appears. 25 is the distance between dots
+    .attr("x", widthBubble * 0.9 + size * .8)
+    .attr("y", (d, i) => i * (size + 5) + (size / 2))
     .style("fill", d => myColor(d))
     .text(d => d)
     .attr("text-anchor", "left")
