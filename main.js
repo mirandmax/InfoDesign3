125c72e1d81bcafd9765b660f7ed1c3b8aa95675
Punti <elias.puntaier@hotmail.com>
2026-06-19 12:52:19 +0200

    resolved typo; removed logs

--- main.js
+++ main.js
@@ -4,12 +4,10 @@ document.addEventListener("DOMContentLoaded", () => {
     "data/gdp-per-capita-worldbank/gdp-per-capita-worldbank.csv"
   ).then((rows) => {
     const year = 2023;
-    console.log(rows.filter((d) => d.code === 'VEN' && d.gini !== null).map(d => d.year));
     const yearGap = 3;
     const currentYearsData = rows
       .filter((d) => d.year >= year - yearGap && d.year <= year + yearGap)
       .filter((d) => d.gini !== null);
-    console.log(currentYearsData);
     drawChoroplethMap(currentYearsData);
     drawBubblePlot(rows);
   });
